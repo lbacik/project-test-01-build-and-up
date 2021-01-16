9fdcf058b14938af571c8d7a7f59aae665d1e008
Łukasz Bacik <mail@luka.sh>
2021-01-16 10:02:24 +0100

    build optimized

--- build.sh
+++ build.sh
@@ -16,24 +16,35 @@ if [ -z "${BRANCH}" ]; then
     exit 1
 fi
 
-if [[ ! "${BRANCH}" =~ ^[0-9a-z-]+$ ]]; then
-    echo "ERROR! Branch name can contain only lower characters, numbers and hyphens"
-    exit 2
-fi
+#if [[ ! "${BRANCH}" =~ ^[0-9a-z-]+$ ]]; then
+#    echo "ERROR! Branch name can contain only lower characters, numbers and hyphens"
+#    exit 2
+#fi
 
-VOLUME="checkout-${PROJECT_NAME}-${BRANCH}"
+#VOLUME="checkout-${PROJECT_NAME}-${BRANCH}"
 
-"${DIRNAME}/bin/checkout.sh" -b "${BRANCH}" -v "${VOLUME}" || exit 2
+#"${DIRNAME}/bin/checkout.sh" -b "${BRANCH}" -v "${VOLUME}" || exit 2
 
-DOCKER_BUILDER="${DOCKER}" run --rm \
-    -v "${DOCKER_SOCKET}:/var/run/docker.sock" \
-    -v "${VOLUME}:${PROJECT_DIR}" \
-    "lbacik/docker-image-builder docker-image-builder"
+#---
 
-$DOCKER_BUILDER \
-    --images-name-prefix "sg-build-${BRANCH}" \
-    --final-image-name "sg-pimcore:${BRANCH}" \
-    --remove-builds \
-    ${PROJECT_DIR} \
-    ${DOCKER_DIR_INSIDE_CONTAINER}/image/build-dev \
-        ARG:BRANCH="${BRANCH}"
+# opcja alternatywna
+# dind
+# config przy up (?)
+
+
+#DOCKER_BUILDER="${DOCKER}" run --rm \
+#    -v "${DOCKER_SOCKET}:/var/run/docker.sock" \
+#    -v "${VOLUME}:${PROJECT_DIR}" \
+#    "lbacik/docker-image-builder docker-image-builder"
+
+#$DOCKER_BUILDER \
+#    --images-name-prefix "build-${PROJECT_NAME}-${BRANCH}" \
+#    --final-image-name "${PROJECT_NAME}:${BRANCH}" \
+#    --remove-builds \
+#    ${PROJECT_DIR}
+
+####    ${DOCKER_DIR_INSIDE_CONTAINER}/image/build-dev \
+####        ARG:BRANCH="${BRANCH}"
+
+${DOCKER} build -t ${PROJECT_NAME}:${BRANCH} ${PROJECT_GIT_URL}#${BRANCH}
+
